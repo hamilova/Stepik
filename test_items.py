c70a241e4f312368b63e9c0d865b857517fe9303
hamilova <hamilovaevgeniya@gmail.com>
2023-08-13 10:44:44 +0300

    conftest and test_items files added

---
+++ test_items.py
@@ -0,0 +1,16 @@
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from conftest import item_contains_add_to_cart_button
+
+
+def test_cart(browser):
+    item_contains_add_to_cart_button(browser)
+    time.sleep(30)
+    add_to_cart_button = WebDriverWait(browser, 60).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn-primary")))
+    assert add_to_cart_button , "корзинка не найдена"
+
